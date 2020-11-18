551ba998f37cd0c0dfcf4c4dd41079bdc031bc38
Feras Hathaf <fhathaf@gmail.com>
2020-11-18 18:05:23 +0000

    created script to trigger creating a .csv and appending to it in the editor

---
+++ Assets/Scripts/MyTools.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class MyTools
+{
+
+    [MenuItem("My Tools/1. Add Defaults To Report %F1")]
+    static void DEV_AppendDefaultsToReport()
+    {
+        CSVManager.AppendToReport(
+            Random.Range(0, 200).ToString()
+        );
+        EditorApplication.Beep();
+        Debug.Log("<color=green>Report updated!</color>");
+    }
+
+    [MenuItem("My Tools/2. Reset Report %F12")]
+    static void DEV_ResetReport()
+    {
+        CSVManager.CreateReport();
+        EditorApplication.Beep();
+        Debug.Log("<color=orange>The report has been reset...</color>");
+    }
+}
