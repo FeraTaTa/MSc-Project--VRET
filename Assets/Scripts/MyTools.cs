516a339fade5c21445c99f98e46571dbf379d8e3
Feras <fhathaf@gmail.com>
2021-01-14 14:18:53 +0000

    added tool to append  filtered heart rate data to a file from the editor

--- Assets/Scripts/MyTools.cs
+++ Assets/Scripts/MyTools.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 
 public static class MyTools
 {
-
+    
     [MenuItem("My Tools/1. Add Defaults To Report %F1")]
     static void DEV_AppendDefaultsToReport()
     {
@@ -21,4 +21,18 @@ public static class MyTools
         EditorApplication.Beep();
         Debug.Log("<color=orange>The report has been reset...</color>");
     }
+
+    [MenuItem("My Tools/3. Append filtered data %F2")]
+    static void DEV_AppendfilteredToReport()
+    {
+        CSVManager.CreateReport();
+        var qwer = GameObject.FindObjectOfType<Filtering>();
+        CSVManager.AppendToReport("LPF: " + qwer.LowPassFilterFactor.ToString());
+        CSVManager.AppendToReport("Ignore: " + qwer.IgnoreReadingFactor.ToString());
+
+        CSVManager.AppendFilteredData(qwer.filteredData);
+        EditorApplication.Beep();
+        Debug.Log("<color=green>Report updated!</color>");
+    }
+
 }
